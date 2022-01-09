a7950f5368da21f4319dfc95eb24ef9fce62f061
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2022-01-09 17:19:30 -0500

    Update 4.intro_to_method_overrides_str.py

--- 08-Object_Oriented_Programing/4.intro_to_method_overrides_str.py
+++ 08-Object_Oriented_Programing/4.intro_to_method_overrides_str.py
@@ -1,5 +1,12 @@
 ########## __str__ ##########
+#By default anytime we print an object it gives a string representation, which gives you a type
+#and a memory address.
 
+#for b in Book.favorites: 
+   # print(b)
+
+#This is not the pretiest representation, so we can actually ovverdie this 
+#and create our own. To do that we can use the str initilaizer (__str__), and pass in self 
 class Book():
     favorites = [] #class level attribute/variable
 
@@ -11,11 +18,13 @@ class Book():
         if self.pages < 100:
             return true
 
+    
     #What happens when you pass object to print?
-    #here we can define a string representaiton of our objects (book1 and book2)
+    #Here we can define a string representaiton of our objects (book1 and book2). All we have to do is return a string 
+    #We are createing an object with , str initilaizer (__str__), and passing in the self variable
     def __str__(self):
-        return "5"
-        return f"{self.title}, {self.pages} pages long"
+        #return "5"
+        return f"{self.title}, is {self.pages} pages long"
 
 #we are creating objects named book and book1 here using the Book() class
 book = Book("The Kite Runner", 168)
@@ -26,10 +35,13 @@ Book.favorites.append(book)
 Book.favorites.append(book2)
 
 #Favorites is related to books, but not tied to one individual book
-#We use self as a parameter if it's unique to each object :)
+print("We use self as a parameter if it's unique to each object :)")
 print(Book.favorites)
 
-#for every single book in this list we are going to print the tittle attribute
+print("for every single book in this list we are going to print the tittle attribute:")
+#for every single book in this list we are now going to print string representaitno of the attributes 
+#becasue we have defined that in the __str__ object created earlier. 
+#when we pass an jobect to print it is automatically goint to invoke the __str__ methord on the object.
 for b in Book.favorites:
-    print(b.title)
+    print(b)
 
