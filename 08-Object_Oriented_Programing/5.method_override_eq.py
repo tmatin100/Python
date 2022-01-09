b5e37e66b024a58e9583a6d85cab0284278377f2
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2022-01-09 18:00:37 -0500

    Update 5.method_override_eq.py

--- 08-Object_Oriented_Programing/5.method_override_eq.py
+++ 08-Object_Oriented_Programing/5.method_override_eq.py
@@ -1,4 +1,5 @@
- 
+######################## __eq__ ###############################
+#We going to use the __eq__ method to compare two book objects. 
 
 class Book():
     favorites = [] #class level attribute or variable
@@ -11,14 +12,51 @@ class Book():
         if self.pages < 100:
             return true
 
+    def __str__(self):
+        #return "5"
+        return f"{self.title}, is {self.pages} pages long"
 
+book = Book("The Kite Runner", 168)
+book2 = Book("The Alchemist", 72)
 
- #What happens when you use ==?
-    def __eq__(self, other):
-        if(self.title == other.title and self.pages == other.pages):
-            return True    
+#Lets say we are comparing the two book objects, the default behavior of the == is to see if they are using the same area of the 
+#memory or if they are the same exact object.
 
+#Therefore, we get a false becasue these objects are in not in the same area of the memory. 
+print(book == book2)
 
+#in order for that to happen we have to set book2 = book
+book2 = book
+#and now we get a value of True since its pointing to the same location in the memroy
+print(book == book2)
+
+#However, thats not what we want, and would rather compare it by the tittle and the page number (methods/attributes). 
+#Lets compare two books, but lets say they have the exact same data. Printing it now we get a False, however, we want it to be true in this 
+#situation since they are the same exact books even though tye are diffent objects. 
 book = Book("The Alchemist", 72)
 book2 = Book("The Alchemist", 72)
+
+#priting it now, it is the same book, however that is not true, since they are two diffent book objects. 
+print(book == book2) 
+
+#We are going to check if the pages and titles of self vs. the other book is the same using the __eq__ constructor
+#we are going to create a self attribute for book one and other attribute for book2
+#Lets check if self.title is equal to other.tittle, and do the same for it's pages, if this is the case we return True, otherwise False. 
+    #What happens when you use ==?
+    def __eq__(self, other):
+        if(self.title == other.title and self.pages == other.pages):
+            return True
+     return Flase 
+    
+#comparison is now true true  since they have the same attributes such as title and pages. 
+book = Book("The Alchemist", 72)
+book2 = Book("The Alchemist", 72)
+
+print(book == book2)
+
+#just to check it we can use two diffent books, we get a false, since they have diffent attrbutes such as title and pages. 
+book = Book("The Alchemist", 72)
+book2 = Book("Not the same book", 72)
+
+print(book == book2)
 
