07294ece2d2fc897b35c38f417e895ddbd095534
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2022-01-09 20:38:06 -0500

    Add files via upload

--- 08-Object_Oriented_Programing/6.hash_and_collections.py
+++ 08-Object_Oriented_Programing/6.hash_and_collections.py
@@ -6,6 +6,12 @@
 #  -Objects hashed using hash() are irreversible, leading to loss of information.
 #  -hash() returns hashed value only for immutable objects, hence can be used as an indicator to check for mutable/immutable objects.
 
+#The best practice when we override the  equals method ( __eq__ ) it best practice to deal with the hash method (__hash__).
+#  We can either create our own custom implementaiton for it, or we can simply state that we are not going to use the hash methord by seting it to None. 
+# __hash_ = None is the default when we override __eq__ (Book is not hashable)
+# The hash method is typically used for any hashing data structres, such as Sets and Dictionaries. 
+
+
 class Book():
     favs = [] #class
 
@@ -15,39 +21,62 @@ class Book():
 
     def is_short(self):
         if self.pages < 100:
-            return true
+            return True
 
-    #What happens when you pass object to print?
     def __str__(self):
         return f"{self.title}, {self.pages} pages long"
 
-    #What happens when you use ==?
     def __eq__(self, other):
         if(self.title == other.title and self.pages == other.pages):
             return True
-    
-    #It's approriate to give something for __hash__ when you override __eq__
-    # #This is the recommended way if mutable (like it is here):
-    __hash__ = None
-
-    #If should immutable, you could do something like this. 
-    #This replaces __hash__ = None
+        return False 
+        
+#This is how we would implement hash, when the data is immuatable only. #This replaces __hash__ = None
     def __hash__(self):
         return hash(self.title) ^ hash(self.pages) #xor with hash of attributes
         #from Mastering Object-Oriented Python
 
 book = Book("Are You My Mother", 72)
-print(book)
+book2 = Book("Not the same", 72)
 
-equal_book = Book("Are You My Mother", 72)
-print("Are they considered equal?", book == equal_book) #yep
-print("Are they the same object?", book is equal_book) #nope
+print(book == book2)
 
-book2 = Book("The Digging-est Dog", 72)
-print(hash(book), hash(book2))
+#print the hash for book object 
+print(hash(book))
 
-print("old hash", hash(book))
+#lets change the date attribute inside the book object and check its hash, notice it will change which is a problem. We dont 
+#want our hashes to change to maintain data integrity. 
+book.title = "Something else"
+print(hash(book))
 
-book.title = "new"
-print("new hash", hash(book)) #BAD!!! 
-#Hashes shouldn't change
+# If two objects are considered equal based on attributes, then they should have the same hash.
+#Therefore the value shoudl be True. 
+
+book3 = Book("The Alchemist", 72)
+book4 = Book("The Alchemist", 72)
+print (book3 == book4)
+print (hash(book3) == hash(book4))
+
+# you can now use those data inside a set
+books = {"book", "book2"}
+
+#in a dictinary only the key is hashed, not the value. 
+#data = {"hello": "hi"}
+
+# List is a type that is not hahsable
+# data2 = {[]]: book} 
+
+#It's approriate to give something for __hash__ when you override __eq__
+# If you dont want your datastructure to be hashable then you use the None type for hash
+# This is basically explicity stating, that this class is not spoosed to be used for hashing
+# #This is the recommended way if the data is mutable, for example updating the books title, pages, etc. 
+#Typically, the hashes derive from the data its storing, so if you are updating the data, then the hash is also changeing.
+# Therefore, its is not recomend to use hashing for mutable data. Or, if you are really carefull you can use hashing for mutable data for hashing, but you should not be changeing it.
+#We doo not change objects that are being used inside a dictionary.
+
+__hash__ = None
+
+##################################Sumamary###########################################
+#1. Hashes should not change, an object hash should remain the same through the entire execution of the code.
+#2. If two objects are considered equeal, then their hashes shoudl be the same. 
+#3. If two objects are diffent but, somehow coicentially their hashes are the same, that is okay. Their hashes do not have to be diffent although that is ideal. 
